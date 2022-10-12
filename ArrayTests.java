6b1d9a91f08e0eeaa4359658d7d593fc7db82563
stareen1 <stareen@ucsd.edu>
2022-10-12 15:21:52 -0700

    fixed reverse methods, actually pass now

--- ArrayTests.java
+++ ArrayTests.java
@@ -2,6 +2,7 @@ import static org.junit.Assert.*;
 import org.junit.*;
 
 public class ArrayTests {
+  
 	@Test 
 	public void testReverseInPlace() {
     int[] input1 = { 3 };
@@ -28,4 +29,11 @@ public class ArrayTests {
     ArrayExamples.reverseInPlace(input1);
     assertArrayEquals(new int[]{4, 3, 2, 1}, input1);
   }
+
+  @Test
+  public void testAverageWithoutOneLowest() {
+    double[] input1 = {1, 2, 3, 1};
+    assertEquals(2.0, ArrayExamples.averageWithoutLowest(input1));
+  }
+  
 }
